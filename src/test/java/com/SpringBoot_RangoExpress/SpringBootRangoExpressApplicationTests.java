edc16feafd96146981786f2ecdf433699cf627a5
Iot-Health-Br <igl_macedo@hotmail.com>
2024-11-18 22:38:30 -0300

    Código Inicial

---
+++ src/test/java/com/SpringBoot_RangoExpress/SpringBootRangoExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_RangoExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRangoExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
